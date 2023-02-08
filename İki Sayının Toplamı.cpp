a55f6524afb946f19700638fc29bb9e1bba3e32f
Radamency <bedirhanyildirim61@gmail.com>
2023-02-08 17:34:22 +0300

    Create İki Sayının Toplamı.cpp

---
+++ İki Sayının Toplamı.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main()
+{
+	int sayi1, sayi2;
+	
+	std::cout << "Birinci Sayiyi Giriniz:";
+	std::cin >> sayi1;
+	
+	std::cout << "Ikinci Sayiyi Giriniz:";
+	std::cin >> sayi2;
+	
+	std:: cout << sayi1 + sayi2;
+	
+	return 0;
+}
